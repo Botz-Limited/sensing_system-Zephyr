49e42ce56f4121f790b5b001c5cf8b6be69300e1
giorgio <giorgio@botzinnovation.com>
2025-07-01 21:17:22 +0000

    Remove build_primary and external directories from git tracking
    These directories should not be tracked as they contain:
    - build_primary: Build artifacts that are regenerated
    - external: West-managed external modules
    
    Both are already in .gitignore but were previously tracked.

--- build_primary/ipc_radio/modules/libmetal/libmetal/lib/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ee/ncs/modules/hal/libmetal/libmetal/lib
-# Build directory: /home/ee/sensing_fw/build_primary/ipc_radio/modules/libmetal/libmetal/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("compiler")
-subdirs("processor")
-subdirs("system")
--- build_primary/ipc_radio/modules/open-amp/open-amp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ee/ncs/modules/lib/open-amp/open-amp
-# Build directory: /home/ee/sensing_fw/build_primary/ipc_radio/modules/open-amp/open-amp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
