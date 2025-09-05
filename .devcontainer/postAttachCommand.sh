e81da11f03b4858a8a5b1ce742ccc6932fbe57bf
giorgio <giorgio@botzinnovation.com>
2025-09-05 13:50:16 +0000

    working on SD Card integration

---
+++ .devcontainer/postAttachCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git config --global --add core.pager "less -F -X"
+git config --global --add safe.directory /home/ee/sensing_fw
+
+west sdk install
+
+\cp -rf /home/ee/sensing_fw/tools/nrfutil /usr/bin/
+
+west update
